6f1d17447845abc56f9dffa5c606a448c9beee02
NoeJDiaz <diaz.noelia.j@gmail.com>
2023-08-08 19:34:26 -0300

    homebanking functionality

---
+++ homebanking/src/main/java/com/mindhub/homebanking/otros/ClientDTO.java
@@ -0,0 +1,62 @@
+package com.mindhub.homebanking.otros;
+
+
+
+import com.mindhub.homebanking.models.Account;
+import com.mindhub.homebanking.models.Client;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ClientDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private Set<AccountDTO> account = new HashSet<>();
+
+
+    public ClientDTO (Client client){
+        this.id = client.getId();
+        this.firstName = client.getFirstName();
+        this.lastName = client.getLastName();
+        this.email = client.getEmail();
+        this.account = client.getAccounts().stream().map(account -> new AccountDTO(account)).collect(Collectors.toSet());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void SetName (String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void SetLastName (String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void SetEmail (String email){
+        this.email = email;
+    }
+
+
+    public Set<AccountDTO> getAccounts() {
+        return account;
+    }
+
+}
